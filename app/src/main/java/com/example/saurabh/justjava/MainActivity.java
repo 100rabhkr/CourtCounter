f3f29cde56f413e1d30739cbbd075516b4a8edd3
100rabhkr <skkumar.sk94@gmail.com>
2016-11-16 23:04:14 +0530

    Second commit

--- app/src/main/java/com/example/saurabh/justjava/MainActivity.java
+++ app/src/main/java/com/example/saurabh/justjava/MainActivity.java
@@ -50,9 +50,9 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void display(int num) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + num);
     }
 
     /**
